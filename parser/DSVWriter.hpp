790cb1e127adca2291b5da24b78ac7a7f1ca6c8e
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-03-20 22:28:46 +0100

    Finish DSVWriter.hpp

--- parser/DSVWriter.hpp
+++ parser/DSVWriter.hpp
@@ -3,15 +3,14 @@
 
 #include <array>
 #include <string>
-
-#include "./LineWriter.hpp"
+#include <fstream>
 
 namespace utility {
 
 inline namespace io {
 
 template<int N, char DELIMITER, char ESCAPE = '\0'>
-class DSVWriter final : private LineWriter {
+class DSVWriter final {
 
 public:
 
@@ -21,22 +20,55 @@ public:
 
     DSVWriter(void)             = delete;
     DSVWriter(const DSVWriter&) = delete;
-    DSVWriter(DSVWriter&&)      = delete;
 
-    auto writeRow(const Row_T& row) -> void; 
+    DSVWriter(DSVWriter&&) = default;
 
-    ~DSVWriter(void) noexcept;
+    auto writeRow(const Row_T& row) noexcept -> void;
+
+    ~DSVWriter(void) noexcept = default;
 
 private:
 
-
+    std::ofstream output;
 
 };
 
 template<int N>
 using CSVWriter = DSVWriter<N, ';', '"'>;
 template<int N>
-using TSVWriter = DSVWriter<N, '\t'>
+using TSVWriter = DSVWriter<N, '\t'>;
+
+template<int N, char DELIMITER, char ESCAPE>
+DSVWriter<N, DELIMITER, ESCAPE>::DSVWriter(const std::string& filename) noexcept {
+    output = std::ofstream(filename);
+}
+
+template<int N, char DELIMITER, char ESCAPE>
+auto DSVWriter<N, DELIMITER, ESCAPE>::writeRow(const Row_T& row) noexcept -> void {
+    using namespace std;
+
+    auto line = string{};
+
+    for (const auto& col : row) {
+        if constexpr (ESCAPE != '\0') {
+            line.push_back(ESCAPE);
+        
+            for (auto c : col) {
+                if (c == ESCAPE) line.push_back(ESCAPE);
+                line.push_back(c);
+            }
+    
+            line.push_back(ESCAPE);
+            line.push_back(DELIMITER);
+        } else {
+            line += col;
+            line.push_back(DELIMITER);
+        }
+    }
+    
+    line.back() = '\n';
+    output << line;
+}
 
 }
 
