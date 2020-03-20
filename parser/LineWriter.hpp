10cd3599c3bc455f2be9f75d4c319863534a0a2f
Naomi Poort <naomi.roberta.ans.poort@gmail.com>
2020-03-20 22:04:46 +0100

    Cleanup

--- parser/LineWriter.hpp
+++ parser/LineWriter.hpp
@@ -1,6 +1,8 @@
 #ifndef UTILITY_IO_LINEWRITER_HEADER
 #define UTILITY_IO_LINEWRITER_HEADER
 
+#include <iostream>
+#include <fstream>
 #include <string>
 
 namespace utility {
@@ -11,7 +13,7 @@ class LineWriter {
 
 public:
 
-    LineWriter(const std::string& filename) noexcept;
+    LineWriter(const std::string& _filename) noexcept;
 
     LineWriter(void)              = delete;
     LineWriter(const LineWriter&) = delete;
@@ -19,10 +21,23 @@ public:
 
     auto writeLine(const std::string& line) noexcept -> void;
 
-    ~LineReader(void) noexcept;
+    ~LineWriter(void) noexcept;
 
 private:
-
+	std::ofstream file;
+	
+	struct Buffer {
+    
+        static constexpr auto MAX_BUFFER_SIZE = 4096;
+    
+        char data[MAX_BUFFER_SIZE];
+    
+        uint16_t position = 0;
+        uint16_t size = 0;
+    
+    } buffer;
+	
+	auto pushBuffer() noexcept -> void;
 };
 
 }
