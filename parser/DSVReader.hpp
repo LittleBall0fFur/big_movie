ab5c292b4616c17f0b52aa5bb751102f58d78282
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-03-24 00:02:43 +0100

    Change DSVReader to use mapped_file
    Changes the DSVReader class to use Boost's 'mapped_file' instead of
    'ifstream', improving the performance. Speed-up increases for larger
    files; from 70 seconds to 44 for the largest file (1.58GB), measured on an
    SSD.

--- parser/DSVReader.hpp
+++ parser/DSVReader.hpp
@@ -2,12 +2,15 @@
 #define UTILITY_IO_DSVREADER_HEADER
 
 #include <array>
-#include <fstream>
 #include <string>
+
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/iostreams/stream.hpp>
 
 namespace utility {
 
 inline namespace io {
+
 
 template<int N, char DELIMITER, char ESCAPE = '\0'>
 class DSVReader final {
@@ -16,64 +19,79 @@ public:
 
     using Row_T = std::array<std::string, N>;
 
-    DSVReader(const std::string& filename) noexcept : input(filename) {}
+    DSVReader(const std::string& filename) noexcept;
 
     DSVReader(void) = delete;
     DSVReader(const DSVReader&) = delete;
     DSVReader(DSVReader&&) = default;
 
-    auto hasRows(void) noexcept -> bool {
-        return input.peek() != EOF;
-    }
+    auto hasRows(void) noexcept -> bool;
 
-    auto readRow(void) noexcept -> Row_T {
-        std::string inputString;
-        getline(input, inputString);
-
-        Row_T array;
-        int currentIndex = 0;
-        bool fill_index = false;
-
-        for(int i = 0; i < inputString.size(); ++i) {
-            switch (inputString[i]) {
-            case DELIMITER:
-                if(!fill_index){
-                    currentIndex++;
-                } else {
-                    array[currentIndex] += inputString[i];;
-                }
-                break;
-
-            case ESCAPE:
-                if((i+1) >= inputString.size() || inputString[(i+1)] != ESCAPE){
-                    fill_index = !fill_index;
-                    break;
-                }
-                array[currentIndex] += inputString[i];
-                i += 1;
-                break;
-
-            default:
-                array[currentIndex] += inputString[i];
-                break;
-            }
-        }
-
-        return array;
-    }
+    auto readRow(void) noexcept -> Row_T;
 
     ~DSVReader(void) noexcept = default;
 
 private:
 
-    std::ifstream input;
+    boost::iostreams::stream<boost::iostreams::mapped_file_source> input;
 
 };
+
 
 template<int N>
 using CSVReader = DSVReader<N, ',', '"'>;
 template<int N>
 using TSVReader = DSVReader<N, '\t'>;
+
+
+template<int N, char DELIMITER, char ESCAPE>
+DSVReader<N, DELIMITER, ESCAPE>::DSVReader(const std::string& filename) noexcept
+    : input(boost::iostreams::mapped_file_params{filename}) {
+    // Empty.        
+}
+
+template<int N, char DELIMITER, char ESCAPE>
+auto DSVReader<N, DELIMITER, ESCAPE>::hasRows(void) noexcept -> bool {
+    return input.peek() != EOF;
+}
+
+template<int N, char DELIMITER, char ESCAPE>
+auto DSVReader<N, DELIMITER, ESCAPE>::readRow(void) noexcept -> Row_T {
+    std::string inputString;
+    getline(input, inputString);
+
+    Row_T array;
+    int currentIndex = 0;
+    bool fill_index = false;
+
+    for(int i = 0; i < inputString.size(); ++i) {
+        switch (inputString[i]) {
+        case DELIMITER:
+            if(!fill_index){
+                currentIndex++;
+            } else {
+                array[currentIndex] += inputString[i];;
+            }
+            break;
+
+        case ESCAPE:
+            if((i+1) >= inputString.size() || inputString[(i+1)] != ESCAPE){
+                fill_index = !fill_index;
+                break;
+            }
+            array[currentIndex] += inputString[i];
+            i += 1;
+            break;
+
+        default:
+            array[currentIndex] += inputString[i];
+            break;
+        }
+    }
+
+    return array;
+}
+
 
 }
 
