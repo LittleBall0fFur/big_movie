361b753d1bba1a4f53d630c44607dc9e5329d977
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-03-27 17:21:02 +0100

    Fix SQL Table definitions

--- table_definitions.sql
+++ table_definitions.sql
@@ -1,4 +1,4 @@
-CREATE TABLE person (
+CREATE TABLE IF NOT EXISTS person (
 	id char(10) NOT NULL,
 	name varchar(255) NOT NULL,
 	birth_year int,
@@ -7,7 +7,7 @@ CREATE TABLE person (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE title (
+CREATE TABLE IF NOT EXISTS title (
 	id char(10) NOT NULL,
 	type varchar(32) NOT NULL,
 	primary_title varchar(512) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE title (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE person$titles (
+CREATE TABLE IF NOT EXISTS person$titles (
 	person_id char(10) NOT NULL,
 	title_id char(10) NOT NULL,
 	PRIMARY KEY (person_id, title_id),
@@ -33,7 +33,7 @@ CREATE TABLE person$titles (
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE episode (
+CREATE TABLE IF NOT EXISTS episode (
 	id char(10) NOT NULL,
 	parent_id char(10) NOT NULL,
 	season_number int,
@@ -49,14 +49,14 @@ CREATE TABLE episode (
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE title$rating (
+CREATE TABLE IF NOT EXISTS title$rating (
 	title_id char(10) NOT NULL,
 	average_rating real NOT NULL,
 	votes int NOT NULL,
 	PRIMARY KEY (title_id)
 );
 
-CREATE TABLE title$akas (
+CREATE TABLE IF NOT EXISTS title$akas (
 	title_id char(10) NOT NULL,
 	ordering int NOT NULL,
 	title varchar(1024) NOT NULL,
@@ -69,24 +69,24 @@ CREATE TABLE title$akas (
 	FOREIGN KEY (title_id)
 		REFERENCES title(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE title$writers (
+CREATE TABLE IF NOT EXISTS title$writers (
 	title_id char(10) NOT NULL,
 	person_id char(10) NOT NULL,
 	PRIMARY KEY (title_id, person_id),
 	FOREIGN KEY (title_id)
 		REFERENCES title(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE,
+		ON UPDATE CASCADE,
 	FOREIGN KEY (person_id)
 		REFERENCES person(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE title$principals (
+CREATE TABLE IF NOT EXISTS title$principals (
 	title_id char(10) NOT NULL,
 	ordering int NOT NULL,
 	person_id char(10) NOT NULL,
@@ -97,33 +97,33 @@ CREATE TABLE title$principals (
 	FOREIGN KEY (title_id)
 		REFERENCES title(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE,
+		ON UPDATE CASCADE,
 	FOREIGN KEY (person_id)
 		REFERENCES person(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE title$directors (
+CREATE TABLE IF NOT EXISTS title$directors (
 	title_id char(10) NOT NULL,
 	person_id char(10) NOT NULL,
 	PRIMARY KEY (title_id, person_id),
 	FOREIGN KEY (title_id)
 		REFERENCES title(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE,
+		ON UPDATE CASCADE,
 	FOREIGN KEY (person_id)
 		REFERENCES person(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE title$genres (
+CREATE TABLE IF NOT EXISTS title$genres (
 	title_id char(10) NOT NULL,
-	genre varchar(32) NOT NULL
+	genre varchar(32) NOT NULL,
 	PRIMARY KEY (title_id),
 	FOREIGN KEY (title_id)
 		REFERENCES title(id)
 		ON DELETE CASCADE
-		ON CASCADE CASCADE
+		ON UPDATE CASCADE
 );
