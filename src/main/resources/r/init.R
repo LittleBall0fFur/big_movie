f82ca33c013abccfd28080decc93f4f1f3c68a15
Sander Albers <34623185+shcalbers@users.noreply.github.com>
2020-04-06 01:14:24 +0200

    Update init.R
    Adds additional dependencies to 'init.R'

--- src/main/resources/r/init.R
+++ src/main/resources/r/init.R
@@ -1,2 +1,4 @@
-dependencies <- c('DBI', 'odbc', 'ggplot2');
+dependencies <- c('DBI', 'odbc', 'ggplot2', 'caTools', 'caret', 'e1071',
+                  'rpart', 'rpart.plot', 'ROCR');
+
 install.packages(dependencies, repos='http://cran.r-project.org');
