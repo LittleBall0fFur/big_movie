3fc135dce85bd9096c5bf198739f8028d6497c73
Sander Albers <34623185+shcalbers@users.noreply.github.com>
2020-04-05 20:50:05 +0200

    Update question_4.sql

--- src/main/resources/sql/questions/question_4.sql
+++ src/main/resources/sql/questions/question_4.sql
@@ -3,7 +3,7 @@
 SELECT
     title$genres.genre,
     title.start_year,
-    AVG(title$rating.average_rating)
+    AVG(title$rating.average_rating) AS average_rating
 FROM
     title INNER JOIN title$genres ON title.id = title$genres.title_id
           INNER JOIN title$rating ON title.id = title$rating.title_id
