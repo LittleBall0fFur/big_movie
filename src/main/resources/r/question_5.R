bdcaa98d55d6cdbf6487cece4c4ca45a8984b817
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-04-05 21:09:34 +0200

    Inline "source('./bmdb.R')" call

--- src/main/resources/r/question_5.R
+++ src/main/resources/r/question_5.R
@@ -1,6 +1,25 @@
 library(ggplot2);
 
-source('./bmdb.R');
+# Include 'bmdb.R' - BEGIN
+library(DBI);
+
+executeQuery <- function (query) {
+  connection <- DBI::dbConnect(odbc::odbc(),
+                               driver = 'PostgreSQL ANSI(x64)',
+                               server = '84.86.32.123',
+                               port   =  5432,
+                               uid    = 'bmdb',
+                               pwd    = 'B1g_M0v13_D4t4b4s3_P455w0rd_F0r_5ch00l');
+
+  result <- DBI::dbSendQuery(connection, query);
+  data <- DBI::dbFetch(result);
+  DBI::dbClearResult(result);
+
+  DBI::dbDisconnect(connection);
+
+  return(data);
+};
+# Include 'bmdb.R' - END
 
 data <- executeQuery("
     SELECT
