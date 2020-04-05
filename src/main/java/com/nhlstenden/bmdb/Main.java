69d4929cfcebaaa1e470777891f648c7d56285de
Naomi Poort <naomi.roberta.ans.poort@gmail.com>
2020-04-05 18:57:10 +0200

    Added all question scene's with text and plots from files

--- src/main/java/com/nhlstenden/bmdb/Main.java
+++ src/main/java/com/nhlstenden/bmdb/Main.java
@@ -8,6 +8,11 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main extends Application {
 
     public static void main(String args[]) {
@@ -23,15 +28,25 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage _stage) {
+    public void start(Stage _stage) throws IOException, URISyntaxException {
         SceneManager.getInstance().init(_stage, "Windows Title", 800, 600, false);
 
-        Scene sc1 = SceneFactory.createTempScene("Scene 1");
-        Scene scq2 = SceneFactory.createQuestionScreen("Question Screen", "graph.png", "Alot of text...");
-        Scene sc3 = SceneFactory.createTempScene("Scene 3");
-        Scene sc4 = SceneFactory.createTempScene("Scene 4");
+        Scene sc1 = SceneFactory.createTempScene("Welcome!");
 
-        SceneManager.getInstance().addScenes(sc1, scq2, sc3, sc4);
+        Scene scq1 = SceneFactory.createQuestionScreen("Question Screen", "question_1");
+        Scene scq2 = SceneFactory.createQuestionScreen("Question Screen", "question_2");
+        Scene scq3 = SceneFactory.createQuestionScreen("Question Screen", "question_3");
+        Scene scq4 = SceneFactory.createQuestionScreen("Question Screen", "question_4");
+        Scene scq5 = SceneFactory.createQuestionScreen("Question Screen", "question_5");
+        Scene scq6 = SceneFactory.createQuestionScreen("Question Screen", "question_6");
+        Scene scq7 = SceneFactory.createQuestionScreen("Question Screen", "question_7");
+        Scene scq8 = SceneFactory.createQuestionScreen("Question Screen", "question_8");
+        Scene scq9 = SceneFactory.createQuestionScreen("Question Screen", "question_9");
+        Scene scq10 = SceneFactory.createQuestionScreen("Question Screen", "question_10");
+
+        Scene sc2 = SceneFactory.createTempScene("End");
+
+        SceneManager.getInstance().addScenes(sc1 , scq7, sc2);//scq1, scq2, scq3, scq4, scq5, scq6, scq7, scq8, scq9, scq10, sc2);
         SceneManager.getInstance().show();
     }
 
