f1127d4e4b04c5b224dafb8045f2f0ce52baaa41
Sander Albers <34623185+shcalbers@users.noreply.github.com>
2020-04-05 20:51:00 +0200

    Update question_8.sql

--- src/main/resources/sql/questions/question_8.sql
+++ src/main/resources/sql/questions/question_8.sql
@@ -1,6 +1,6 @@
 -- Question #9
 
-SELECT genre, ROUND(AVG(seasons), 0)
+SELECT genre, ROUND(AVG(seasons), 0) AS average_seasons
 FROM (SELECT genre, MAX(season_number) AS seasons
       FROM episode INNER JOIN title$genres ON episode.parent_id = title$genres.title_id
       GROUP BY genre, episode.parent_id) AS episode$seasons
